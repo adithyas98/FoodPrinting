1e3844221700374e4cde0ca2fa718f242493a66c
Adithya Shastry <adi.shastry@gmail.com>
2022-04-15 11:15:27 -0400

    Added the final functions to gcode.py and created pyramid.py

--- src/gcode.py
+++ src/gcode.py
@@ -9,15 +9,47 @@ class GCode:
     for focus on creating complex food structures
     '''
 
-    def __init__(self,numExtruder=2):
-        '''
-        Inputs:
-            - numExtruder: The number of extruders. Default is 2
-        '''
-        self.numExtruder = numExtruder
+    def __init__(self):
         self.absolute = True
         self.absoluteCordinates = None
         self.imperial = False
+    def moveZ(self,Z):
+        '''
+        This method will quick move the nozzle in the Z direction. Depending
+        on the setting it will to the following:
+            Absolute: Will move to the specified location relative to the 
+                    current origin
+            Relative: Will move the nozzle along the Z relative to the current
+                        position
+        Inputs:
+            - Z - the z input for movement
+        Outputs:
+            - the gcode required to take the action
+        '''
+        return "G0 Z{}".format(Z)
+    def startup(self,fanSpeed=255,extruder=0):
+        '''
+        This will handle all of the startup tasks that you need to complete
+        before you start printing
+        Inputs:
+            - fanSpeed: range from 0-255 that controls the fan speed
+        Output:
+            - Code to start up the printing process
+        '''
+        code = ''
+        code += "M106 S{}".format(fanSpeed)
+        return code
+
+    def switchExtruder(self,extruder=0):
+        '''
+        Will switch th extruder to the specified extruder
+        Input:
+            -extruder: a value 0 or 1 to pick the extruder
+        Output:
+            - the code required to change the extruder
+        '''
+        assert extruder in [0,1]
+        return "T{}".format(extruder)
     def line(self,p1,p2,movSpeed,extrude):
         '''
         Will move/draw a line between points p1 and p2
@@ -37,7 +69,7 @@ class GCode:
 
         #Now we can return the gcode
         return code
-    def setAbsolute(self,absolute):
+    def setAbsolute(self,absolute=True):
         '''
         Will change the status of absolute or relative
         Input:
@@ -75,7 +107,7 @@ class GCode:
             - None if no absolute coordinates have been set
         '''
         return self.absoluteCordinates
-    def setImperial(self,imperial):
+    def setImperial(self,imperial=True):
         '''
         Will set the units to imperial (if input is True) and metric
         (if input is False).
@@ -93,6 +125,16 @@ class GCode:
             #we want to set to metric
             return 'G21\n'
 
+    def move(self,point):
+        '''
+        Will move the printer away
+        Input:
+            - point:will move the printer to the point (absolute mode)
+                or by the specified amount (relative mode)
+                point is a tuple (x,y,z)
+
+        '''
+        return "G0 X{} Y{} Z{}".format(point[0],point[1],point[2])
     def getUnits(self):
         '''
         Will get the units currently being used
